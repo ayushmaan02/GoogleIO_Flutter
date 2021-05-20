8b322b6f5187397efe1146f2e74bfbc46f25a972
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2021-05-20 15:54:45 +0530

    commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# gioapp 
+It's a basic flutter project learned from Google I/O session of Building your first Flutter app.
+
+_Learned how to create an infinite scrolling ListView._
+## Project Overview
+![](Google_IO.gif)
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcome to Flutter",
+      home: Scaffold(
+        body: Center(
+          child: Random(),
+        ),
+      ),
+    );
+  }
+}
+
+class Random extends StatefulWidget {
+  @override
+  _RandomState createState() => _RandomState();
+}
+
+class _RandomState extends State<Random> {
+  final suggestion = <WordPair>[];
+  final biggerFont = TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(pair.asPascalCase, style: biggerFont),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= suggestion.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            suggestion.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(suggestion[index]);
+        });
+  }
+}
