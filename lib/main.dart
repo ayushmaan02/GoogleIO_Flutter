055113d95f0070d4f61584526d6558ddbb792fd3
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2021-05-22 21:53:16 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Welcome to Flutter",
+      title: "Strtup Name Generator",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: Scaffold(
         body: Center(
           child: Random(),
